101e6c2335b54c8a7bf08181a585db251b7cecc3
MateusSartorio <mateussartorio39@gmail.com>
2022-09-20 14:20:55 -0300

    final_build

---
+++ traducao_ch_16_pg_17.php
@@ -0,0 +1,9 @@
+urgh
+p-principe Christopher
+porque você está aqui
+(confusa)
+venha agora
+você não acreditou honestamente que eu deixaria Eli entrar em perigo sozinha, achou?
+eu não fiz nada!
+Principe eu te imploro não dê ouvidos aquela pirralha
+
